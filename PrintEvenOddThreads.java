42b75da4e2994bfa1d6b364a241eda4462275fef
PRASANNA KUMAR BILLA <31616311+billaprasanna@users.noreply.github.com>
2020-09-15 14:21:08 +0530

    Update PrintEvenOddThreads.java

--- PrintEvenOddThreads.java
+++ PrintEvenOddThreads.java
@@ -69,3 +69,77 @@ public class OddEvenPrintMain {
     }
 
 }
+
+
+
+//============================================================================
+public class NumbersGenerator {
+ 
+	private int number = 1;
+	private int numberOfThreads;
+	private int totalNumbersInSequence;
+	
+	public NumbersGenerator(int numberOfThreads, int totalNumbersInSequence) {
+		super();
+		this.numberOfThreads = numberOfThreads;
+		this.totalNumbersInSequence = totalNumbersInSequence;
+	}
+	
+	public void printNumbers(int result) {
+		synchronized (this) {
+			while (number < totalNumbersInSequence-1) {
+				while (number % numberOfThreads != result) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				System.out.println(Thread.currentThread().getName()+" "+number++);
+				notifyAll();
+			}
+		}
+	}
+}
+
+public class SequenceGeneratorTask implements Runnable {
+ 
+	private NumbersGenerator numbersGenerator;
+	private int result;
+	
+	public SequenceGeneratorTask(NumbersGenerator numbersGenerator, int result) {
+		super();
+		this.numbersGenerator = numbersGenerator;
+		this.result = result;
+	}
+ 
+	@Override
+	public void run() {
+		numbersGenerator.printNumbers(result);
+	}
+ 
+}
+
+public class SequenceNumberGeneratorTest {
+ 
+	private static final int TOTAL_NUMBER_IN_SEQUENCE = 10;
+	private static final int TOTAL_NUMBER_OF_THREADS = 3;
+	
+	public static void main(String[] args) {
+		
+		NumbersGenerator numbersGenerator = new NumbersGenerator(TOTAL_NUMBER_OF_THREADS, TOTAL_NUMBER_IN_SEQUENCE);
+		
+		//Created three Threads
+		Thread t1 = new Thread(new SequenceGeneratorTask(numbersGenerator, 1), "Thread-1");
+		Thread t2 = new Thread(new SequenceGeneratorTask(numbersGenerator, 2), "Thread-2");
+		Thread t3 = new Thread(new SequenceGeneratorTask(numbersGenerator, 0), "Thread-3");
+		
+		//Start all three threads
+		t1.start();
+		t2.start();
+		t3.start();
+ 
+	}
+}
+    
