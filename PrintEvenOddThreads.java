5333b93e8ddab84018fa3b8869f3f52fa3157dff
PRASANNA KUMAR BILLA <31616311+billaprasanna@users.noreply.github.com>
2020-09-15 14:15:23 +0530

    Create PrintEvenOddThreads.java

---
+++ PrintEvenOddThreads.java
@@ -0,0 +1,71 @@
+package com.paypal.interview;
+
+public class OddEvenPrintMain {
+    boolean odd;
+    int count = 1;
+    int MAX = 20;
+
+    public void printOdd() {
+        synchronized (this) {
+            while (count < MAX) {
+                while (!odd) {
+                    try {
+                        wait();
+                     } catch (InterruptedException e) {
+                             e.printStackTrace();
+                    }
+                }
+                System.out.println("Odd Thread :" + count);
+                count++;
+                odd = false;
+                notify();
+            }
+        }
+    }
+
+    public void printEven() {
+        synchronized (this) {
+            while (count < MAX) {
+                while (odd) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Even thread :" + count);
+                count++;
+                odd = true;
+                notify();
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final OddEvenPrintMain oep = new OddEvenPrintMain();
+        oep.odd = true;
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                oep.printEven();
+
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                oep.printOdd();
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+    }
+
+}
